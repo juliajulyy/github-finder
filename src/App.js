ce95a177d6362fdaf68d067d5cf1e621cfea068c
Yulia Cheliak <juliacheliak@gmail.com>
2021-02-12 16:03:31 +0200

    add search ability

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 
 class App extends Component {
@@ -11,19 +12,25 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  // this.setState({ loading: true });
+  // }
+  // Search Github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&clien_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-    const res = await axios.get('https://api.github.com/users');
-
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
