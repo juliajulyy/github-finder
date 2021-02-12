e701638f27c5e2d8ac41223b53e30bfe4172459c
Yulia Cheliak <juliacheliak@gmail.com>
2021-02-12 17:59:10 +0200

    add router & about page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({ icon, title }) => {
+const Navbar = ({ icon = 'fab fa-github', title = 'Github Finder' }) => {
   return (
     <nav className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
-};
-
-Navbar.defaultProps = {
-  title: 'Github Finder',
-  icon: 'fab fa-github',
 };
 
 Navbar.propTypes = {
